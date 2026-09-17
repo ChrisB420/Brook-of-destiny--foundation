d9437699da1bfac1b337fdeaf0ef6044a1b87beb
ChrisB420 <christianchriz321@gmail.com>
2026-09-18 00:46:38 +0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -22,7 +22,10 @@ export const Route = createRootRoute({
       { name: "theme-color", content: "#0E3B2E" },
     ],
     links: [
-      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32.png" },
+{ rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16.png" },
+{ rel: "icon", href: "/favicon.ico" },
+{ rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       {
         rel: "preconnect",
