5a012be84f423f27a3a103ec97b893b573740e0d
ChrisB420 <christianchriz321@gmail.com>
2026-09-20 00:44:49 +0300

    Update site.ts

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -133,7 +133,7 @@ export const PROGRAMMES: Programme[] = [
       "Walk with a child. Support their education. Nurture their destiny. Your partnership can help transform a difficult beginning into a journey of hope, learning, opportunity, and possibility.",
     ],
 
-    image: "/images/education/education-01.jpg",
+    image: "/images/education/education-01.jpeg",
 
     imageAlt:
       "Young learners studying with a mentor in a classroom in western Kenya",
